b979d2d1d8fb05cd71abdfc3088124a9c303b3e2
GerardoDM <geradelgado22@hotmail.com>
2022-04-07 17:49:31 -0500

    v1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'cache-1'
+const CACHE_DYNAMIC = 'dynamic-1'
+const CACHE_INMUTABLE = 'inmutable-1'
+
+const APP_SHELL = ['/', 'index.html', 'css/styles.css', 'img/favico.ico', 'js/app.js', 'img/avs/fl3.png', 'img/avs/fl4.png', 'img/avs/fl5.png', 'img/avs/fl6.png']
+const APP_INMUTABLE = ['https://fonts.googleapis.com/css?family=Quicksand:300,400', 'https://fonts.googleapis.com/css?family=Lato:400,300', 'https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css', 'js/libs/jquery']
+
+self.addEventListener('install', event => {
+    const CACHE_APP = caches.open(CACHE_NAME).then(cache => {
+        cache.addAll(APP_SHELL)
+    })
+
+    const CACHEINMUTABLE = caches.open(CACHE_INMUTABLE).then(cache => {
+        cache.addAll(APP_INMUTABLE)
+    })
+
+    event.waitUntil(Promise.all([CACHE_APP, CACHEINMUTABLE]))
+})
+
+self.addEventListener('activate', event => {
+    
+})
+

c0a6ea8aa87dd548d0e1a76525c718d488301bb8
GerardoDM <geradelgado22@hotmail.com>
2022-04-08 13:25:15 -0500

    edit app js

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@
 
 	
 var url = window.location.href; //obtenemos todo el url
-var pwaLocation = '/repositorioGitHub/sw.js'; //path donde se encuentra el sw en GitHub
+var pwaLocation = '/pwa_one/sw.js'; //path donde se encuentra el sw en GitHub
  
 if(navigator.serviceWorker){
     if(url.includes('localhost')){
